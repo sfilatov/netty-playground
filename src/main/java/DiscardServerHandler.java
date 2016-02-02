97d25c538a46d2ddc6a1940a6151d040b1351d93
Stepan Filatov <stepanf@microsoft.com>
2016-02-02 23:08:44 +0100

    discard server

---
+++ src/main/java/DiscardServerHandler.java
@@ -0,0 +1,17 @@
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("Connection ");
+        ReferenceCountUtil.release(msg);
+    }
+}
